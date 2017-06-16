039baba304d5c8d4d6ebfd158066c60676c2367b
iii <iii@userde-MacBook-6.local>
2017-06-16 14:53:56 +0800

    v1

--- RongS09/MyClasses.swift
+++ RongS09/MyClasses.swift
@@ -11,7 +11,47 @@ import Foundation
 class MyClass01 {
     var x: Int
     init(x :Int) {self.x = x}
-    subscript(y:Int) -> Int {
-        return y * x
+    subscript(y:Int) -> String {
+        return "\(x) x \(y) = \(x * y)"
+    }
+    subscript(y:Int, z:Int)->Int{
+        return 123
+    }
+    subscript(y: Double) -> Double{
+        get{
+            print("get")
+            return floor(y)
+        }
+        set {
+            print("set")
+            self.x = Int(ceil(newValue))
+        }
+    }
+}
+//---------------------------
+class Super1 {
+   private var x = 10 //內外看不到
+    func f1() {
+        print("Super1:f1()")
+    }
+    func f2(x:Int) -> Double {
+        print("f2(x:Int)")
+        return 1.0
+    }
+
+}
+class Sub11 : Super1 {
+    var x = 3
+    override func f1() {
+        super.f1() //與編譯無關可加不加 邏輯有關
+    print("Sub11:f1()")
+    }
+    func f2(y:Int) {
+        print("f2(y:Int)")
+    }
+    override func f2(x:Int)->Double {
+        f1() //自己的
+        super.f1() //爸爸的
+        return 1.0
     }
 }
--- RongS09/main.swift
+++ RongS09/main.swift
@@ -9,7 +9,19 @@
 import Foundation
 
 var obj1 = MyClass01(x:2)
-print(obj1[2])
-obj1.x = 3
-print(obj1[7])
+obj1.x = 9
+for i in 1...9 {
+print(obj1[i])
+}
 
+print(obj1[3,4])
+
+print(obj1[3.14]) //get
+obj1[3.14] = 7.456 //get + set
+print(obj1.x)
+
+var obj2 = Sub11()
+print(obj2.x)
+obj2.f1()
+obj2.f2(x: 1)
+obj2.f2(y: 1)
